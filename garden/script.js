51146de57a3c557e6abfd32d460be559933a576b
alejandro-panza <alejandrofpanza@gmail.com>
2023-02-01 17:47:20 -0300

    adds paragraph

--- garden/script.js
+++ garden/script.js
@@ -7,7 +7,7 @@ var layout = {
 
 Plotly.react("single", dataSingle, layout);
 
-Plotly.react("myDiv", data, layout);
+// Plotly.react("myDiv", data, layout);
 
 Plotly.react("bigger", dataBigger, layout);
 Plotly.react("tired", dataTired, layout);
